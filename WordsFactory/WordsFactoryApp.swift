8c78ac07e0615fc6ad38437059c29bc013986133
Arsentiy Gonda <arsentiy.gonda@gmail.com>
2024-04-27 13:48:43 +0700

    [init] Init empty SwiftUI project
    -- Project without Core Data
    -- Project without Tests

---
+++ WordsFactory/WordsFactoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsFactoryApp.swift
+//  WordsFactory
+//
+//  Created by HITSStudent on 27.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordsFactoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
